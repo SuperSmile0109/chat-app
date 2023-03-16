02f1794e5bdf1b9bf8c6096d48feae9fd996f4bf
SuperSmile <superjodev426@gmail.com>
2023-03-16 01:59:23 -0800

    feat: implement the route to chat direct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './features/feature-chat/pages/chat.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  {
+    path: '',
+    component: ChatComponent,
+    children: [
+      {
+        path: 'chat',
+        loadChildren: () =>
+          import('src/app/features/feature-chat/chat.module').then(m => m.ChatModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
