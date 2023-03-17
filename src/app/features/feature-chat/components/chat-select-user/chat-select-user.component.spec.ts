e5bce1613873ac29959a480aa3ff98bc25de3dec
SuperSmile <superjodev426@gmail.com>
2023-03-17 10:15:40 -0800

    feat: add chat-select-channel-component test

---
+++ src/app/features/feature-chat/components/chat-select-user/chat-select-user.component.spec.ts
@@ -0,0 +1,39 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatSelectUserComponent } from './chat-select-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { findEl, setFieldValue } from 'src/app/core/spec-helpers/chat.spec-helper';
+
+describe('ChatSelectUserComponent', () => {
+  let component: ChatSelectUserComponent;
+  let fixture: ComponentFixture<ChatSelectUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ChatSelectUserComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ChatSelectUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit/get selected user', () => {
+    // Spy on event function
+    spyOn(component, 'getSelectedUser');
+
+    // Set values
+    setFieldValue(fixture, 'user-id', 'Russell');
+    fixture.detectChanges();
+
+    // Trigger event
+    findEl(fixture, 'form').triggerEventHandler('change', {});
+    fixture.detectChanges();
+    expect(component.getSelectedUser).toHaveBeenCalled();
+  });
+});
